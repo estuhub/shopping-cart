1340d85ba3c297492f70777c800b294ffe6cd66d
estuhub <107235863+estuhub@users.noreply.github.com>
2023-10-20 10:08:58 +0700

    create path using react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { Routes, Route  } from "react-router-dom"
+import { Home } from './pages/Home.tsx'
+import { Store } from './pages/Store.tsx'
+import { About } from './pages/About.tsx'
+
 function App() {
 
-  return <h1>Hi</h1>
+  return (
+    <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/store" element={<Store />}/>
+        <Route path="/about" element={<About />}/>
+    </Routes>
+  )
 }
 
 export default App
