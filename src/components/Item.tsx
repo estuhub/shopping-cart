814d2982f798835bb67f6cfd76e5884694e1c9cc
estuhub <107235863+estuhub@users.noreply.github.com>
2023-10-21 14:57:23 +0700

    create buttons to add or remove from cart

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap"
+import { Button, Card } from "react-bootstrap"
 import { formatCurrency } from "../utils/formatCurrency"
 
 type ItemsProps = {
@@ -9,14 +9,35 @@ type ItemsProps = {
 }
 
 export function Item({ id, name, price, img }: ItemsProps) {
+    let quantity = 1
     return (
-        <Card>
+        <Card className="h-100">
             <Card.Img variant="top" src={img} height="200px" style={{ objectFit: "cover"}}  />
             <Card.Body>
                 <Card.Title className="d-flex justify-content-between">
-                    <span>Name</span>
+                    <span>{name}</span>
                     <span className="text-muted">{formatCurrency(price)}</span>
                 </Card.Title>
+                <div className="mt-auto">
+                    {
+                        quantity === 0 ? 
+                        <Button className="w-100">+ Add to Cart</Button> :
+                        <div 
+                            className="d-flex align-items-center flex-column"
+                            style={{ gap: "0.5rem"}}
+                        >
+                            <div 
+                                className="d-flex align-items-center" 
+                                style={{ gap: "0.5rem"}}
+                            >
+                                    <Button>-</Button>
+                                    <span className="fs-3">{quantity}</span> in cart
+                                    <Button>+</Button>
+                            </div>
+                            <Button className="border-danger bg-danger">Remove</Button>
+                        </div>
+                    }
+                </div>
             </Card.Body>
         </Card>
     )
