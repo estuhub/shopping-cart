d0d4531d950ff8f940e591c5d018c10dce62fb57
estuhub <107235863+estuhub@users.noreply.github.com>
2023-10-21 14:06:15 +0700

    create cart style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,14 +3,33 @@ import { NavLink } from 'react-router-dom'
 
 export function Navbar() {
     return (
-        <NavbarBs className='shadow-sm mb-3'>
+        <NavbarBs sticky="top" className='shadow-sm mb-3'>
             <Container>
                 <Nav className='me-auto'>
                     <Nav.Link to="/" as={NavLink} className="ps-0">Home</Nav.Link>
                     <Nav.Link to="/about" as={NavLink}>About</Nav.Link>
                     <Nav.Link to="/store" as={NavLink}>Store</Nav.Link>
                 </Nav>
-                <Button>Cart</Button>
+                <Button 
+                    className="rounded-circle bg-white text-primary"
+                    style={{ width: "3rem", height: "3rem", position: "relative"}}
+                    >
+                    C
+                    <div 
+                        className="rounded-circle bg-danger d-flex justify-content-center align-items-center" 
+                        style={{
+                            color: "white",
+                            width: "1.5rem",
+                            height: "1.5rem",
+                            position: "absolute",
+                            bottom: "0",
+                            right: "0",
+                            transform: "translate(25%, 25%)"
+                        }}
+                    >
+                        3
+                    </div>
+                </Button>
             </Container>    
         </NavbarBs>
     )
